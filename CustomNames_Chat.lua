65df16e9352bc35bf6f5b321bf4f87e01f9e886c
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2024-07-29 16:10:48 +0200

    initial addon creation

---
+++ CustomNames_Chat.lua
@@ -0,0 +1,26 @@
+local lib = LibStub("CustomNames")
+local AceHook = LibStub("AceHook-3.0")
+
+
+local function convertName(msg, name)
+	local customName = lib.Get(name)
+	if customName ~= name then
+		local color = msg:match("|c(%x%x%x%x%x%x%x%x)(.-)|r")
+		local colorCodedName = WrapTextInColorCode(customName, color)
+		return "|Hplayer:"..name.."|h["..colorCodedName.."]|h" 
+	end
+end
+
+function AceHook:AddMessage(frame, text, ...)
+	if text and type(text) == "string" then 
+		text = text:gsub("(|Hplayer:([^:]+).-|h.-|h)", convertName)
+	end
+	return self.hooks[frame].AddMessage(frame, text, ...)
+end
+
+for i = 1, NUM_CHAT_WINDOWS do
+	local cf = _G["ChatFrame" .. i]
+	if cf ~= COMBATLOG then
+		AceHook:RawHook(cf, "AddMessage", true)
+	end
+end
